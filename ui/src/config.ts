72bc7984986b4849ab2a901599cc6116621c2771
guillem.cordoba <guillem.cordoba@gmail.com>
2025-03-11 15:17:35 +0100

    Lint

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,6 +1,6 @@
 import { ProfilesConfig } from '@darksoil-studio/profiles-provider';
 
-export interface FriendsConfig extends ProfilesConfig {}
+export type FriendsConfig = ProfilesConfig;
 
 export const defaultConfig: FriendsConfig = {
 	avatarMode: 'avatar-required',
