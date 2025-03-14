6398c639f17f86550540c060b4a69dac2f29a3f9
guillemcordoba <guillemcordoba@users.noreply.github.com>
2025-03-14 11:37:08 +0000

    Deploying to gh-pages from @ darksoil-studio/friends-zome@b827e481bb5238f83d4024e048baefe0a0a013e6 🚀

---
+++ backend/release/deps/friends_integrity-1644e5a55eabb374.d
@@ -0,0 +1,5 @@
+/home/runner/work/friends-zome/friends-zome/docs/public/backend/release/deps/libfriends_integrity-1644e5a55eabb374.rmeta: zomes/integrity/friends/src/lib.rs
+
+/home/runner/work/friends-zome/friends-zome/docs/public/backend/release/deps/friends_integrity-1644e5a55eabb374.d: zomes/integrity/friends/src/lib.rs
+
+zomes/integrity/friends/src/lib.rs:
