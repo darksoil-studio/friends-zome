a329ed217284d0b1efe6d3bf4a026e7046af6c6f
guillemcordoba <guillemcordoba@users.noreply.github.com>
2025-07-02 12:35:22 +0000

    Deploying to gh-pages from @ darksoil-studio/friends-zome@d9fe54ec9a538d08d94f1b87f11f310a13d6a905 🚀

--- backend/doc/src-files.js
+++ backend/doc/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["friends",["",[],["all_friends.rs","friend_request.rs","lib.rs","notifications.rs","private_event.rs","profile.rs"]]],["friends_integrity",["",[],["lib.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["friends",["",[],["all_friends.rs","friend_request.rs","lib.rs","notifications.rs","private_event.rs","profile.rs","profiles_provider.rs"]]],["friends_integrity",["",[],["lib.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[118,41]}
+//{"start":36,"fragment_lengths":[141,41]}
