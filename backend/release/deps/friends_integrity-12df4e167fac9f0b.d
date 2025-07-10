d5c2b14d92e55b2d1a088220bee7d8ff5fac6ba3
guillemcordoba <guillemcordoba@users.noreply.github.com>
2025-07-10 09:21:49 +0000

    Deploying to gh-pages from @ darksoil-studio/friends-zome@438e8dd9729e73a5b94b1a91cdb9cd21be3d8af6 🚀

---
+++ backend/release/deps/friends_integrity-12df4e167fac9f0b.d
@@ -0,0 +1,5 @@
+/home/runner/work/friends-zome/friends-zome/docs/public/backend/release/deps/libfriends_integrity-12df4e167fac9f0b.rmeta: zomes/integrity/friends/src/lib.rs
+
+/home/runner/work/friends-zome/friends-zome/docs/public/backend/release/deps/friends_integrity-12df4e167fac9f0b.d: zomes/integrity/friends/src/lib.rs
+
+zomes/integrity/friends/src/lib.rs:
--- backend/release/deps/profiles_provider_zome_trait-d3e70b9b5180031b.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/friends-zome/friends-zome/docs/public/backend/release/deps/libprofiles_provider_zome_trait-d3e70b9b5180031b.rmeta: /home/runner/.cargo/git/checkouts/profiles-zome-c98c38d5abf63c77/9d0be0e/crates/profiles_provider_zome_trait/src/lib.rs
-
-/home/runner/work/friends-zome/friends-zome/docs/public/backend/release/deps/profiles_provider_zome_trait-d3e70b9b5180031b.d: /home/runner/.cargo/git/checkouts/profiles-zome-c98c38d5abf63c77/9d0be0e/crates/profiles_provider_zome_trait/src/lib.rs
-
-/home/runner/.cargo/git/checkouts/profiles-zome-c98c38d5abf63c77/9d0be0e/crates/profiles_provider_zome_trait/src/lib.rs:
